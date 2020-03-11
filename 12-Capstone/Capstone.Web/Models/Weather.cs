f22e5506b4944a89af21fa6bf6d42ac31f8a1d94
Andrew Burkett <andrewburkett.abb@gmail.com>
2020-03-11 15:43:20 -0400

    these are my lost brain cells

--- 12-Capstone/Capstone.Web/Models/Weather.cs
+++ 12-Capstone/Capstone.Web/Models/Weather.cs
@@ -12,5 +12,6 @@ namespace Capstone.Web.Models
         public int Low { get; set; }
         public int High { get; set; }
         public string Forecast { get; set; }
+        public int Count { get; set; } = 1;
     }
 }
