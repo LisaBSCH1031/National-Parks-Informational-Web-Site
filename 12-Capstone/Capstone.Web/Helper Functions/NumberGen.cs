fc8d25055b9c74b7bbc6800b4959af66dc75ee6f
Andrew Burkett <andrewburkett.abb@gmail.com>
2020-03-12 17:34:23 -0400

    Cool random pictures

---
+++ 12-Capstone/Capstone.Web/Helper Functions/NumberGen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web
+{
+    public static class NumberGen
+    {
+        public static string GetRandom()
+        {
+            Random random = new Random();
+            int randomInt = random.Next(1, 10);
+            return randomInt.ToString();
+        }
+    }
+}
