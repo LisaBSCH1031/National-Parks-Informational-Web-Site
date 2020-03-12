e08e6e1b88a836faa306851e322cf0348bec4fa8
Lisa <lisalynn103187@gmail.com>
2020-03-12 14:42:02 -0400

    Celcius View

--- 12-Capstone/Capstone.Web/Controllers/HomeController.cs
+++ 12-Capstone/Capstone.Web/Controllers/HomeController.cs
@@ -40,7 +40,15 @@ namespace Capstone.Web.Controllers
             return View(vm);
         }
 
-      
+        public IActionResult DetailCelcius(string id, ParkWeatherVM vm)
+        {
+            vm.park = parksDAO.GetPark(id);
+            vm.weather = weatherDAO.GetWeather(id);
+            vm.weatherDays = weatherDAO.GetWeatherDays(id);
+            return View(vm);
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
